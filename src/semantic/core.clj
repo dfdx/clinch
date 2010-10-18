ae7f9ad118cbfc323b5a01b01fd507b9595586ad
Faithless Friend <faithlessfriend@gmail.com>
2010-10-19 01:28:42 +0300

    Removed semantic directory

--- src/semantic/core.clj
+++
@@ -1,1 +0,0 @@
-(ns semantic.core)
--- src/semantic/hello.clj
+++
@@ -1,8 +0,0 @@
-
-(ns semantic.hello
-  (:gen-class
-   :methods [[sayhello [] void]]))
-
-(defn -sayhello [this] (println "Hello from semantics!"))
-
-(defn -main [] (-sayhello nil))
